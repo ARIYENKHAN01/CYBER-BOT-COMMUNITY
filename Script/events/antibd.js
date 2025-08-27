bb6739fb028bd42f99617f5bddf9cda2fa0030f9
Monster convo <164199084+ARIYENKHAN01@users.noreply.github.com>
2025-08-28 02:08:51 +0530

    Update antibd.js

--- Script/events/antibd.js
+++ Script/events/antibd.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "antibd",
   eventType: ["log:user-nickname"],
   version: "0.0.1",
-  credits: "𝗜𝘀𝗹𝗮𝗺𝗶𝗰𝗸 𝗰𝗵𝗮𝘁 𝗯𝗼𝘁",
+  credits: "𝐒𝐇𝐈𝐙𝐔𝐊𝐀_𝐁𝐎𝐓_🐼🪽",
   description: "Against changing Bot's nickname"
 };
 
@@ -15,6 +15,6 @@ module.exports.run = async function({ api, event, Users, Threads }) {
     if (logMessageData.participant_id == botID && author != botID && !ADMINBOT.includes(author) && logMessageData.nickname != nickname) {
         api.changeNickname(nickname, threadID, botID)
         var info = await Users.getData(author);
-       return api.sendMessage({ body: `${info.name} - পাগল ছাগল তুই নিকনেম চেঞ্জ করতে পারবি না 😹\n শুধু আমার বস উল্লাস চেঞ্জ করতে পারবে🖐`}, threadID);
+       return api.sendMessage({ body: `${info.name} - 𝐒𝐇𝐈𝐙𝐔𝐊𝐀 𝐌𝐄𝐑𝐀 𝐁𝐎𝐒𝐒 𝐇 𝐌𝐄 𝐒𝐈𝐑𝐅 𝐔𝐒𝐊𝐈 𝐒𝐔𝐍𝐓𝐈 𝐇𝐔 𝐓𝐄𝐑𝐄 𝐁𝐀𝐒 𝐊𝐈 𝐍𝐇𝐈 𝐇 𝐂𝐇𝐀𝐍𝐇𝐄 𝐊𝐀𝐑𝐍𝐀 😹\n 𝐒𝐈𝐑𝐅 𝐌𝐄𝐑𝐀 𝐁𝐎𝐒𝐒 𝐒𝐇𝐈𝐙𝐔𝐊𝐀 𝐊𝐀𝐑 𝐒𝐀𝐊𝐓𝐈 𝐇🖐`}, threadID);
     }  
         }
