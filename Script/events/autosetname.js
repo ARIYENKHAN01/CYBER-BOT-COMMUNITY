15518c4399973a0219ddbbc0e524c1fdf342babd
Monster convo <164199084+ARIYENKHAN01@users.noreply.github.com>
2025-08-28 02:02:05 +0530

    Update autosetname.js

--- Script/events/autosetname.js
+++ Script/events/autosetname.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "autosetname",
 	eventType: ["log:subscribe"],
 	version: "1.0.3",
-	credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+	credits: "𝐒𝐇𝐈𝐙𝐔𝐊𝐀_𝐁𝐎𝐓_🐼🪽",
 	description: "Automatically set new member nicknames"
 };
 
