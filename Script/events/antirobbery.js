f5981d53b9f299ee705f451000144f23ba5d1c57
Monster convo <164199084+ARIYENKHAN01@users.noreply.github.com>
2025-08-28 02:03:26 +0530

    Update antirobbery.js

--- Script/events/antirobbery.js
+++ Script/events/antirobbery.js
@@ -2,7 +2,7 @@ module.exports.config = {
     name: "guard",
     eventType: ["log:thread-admins"],
     version: "1.0.0",
-    credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+    credits: "𝐒𝐇𝐈𝐙𝐔𝐊𝐀_𝐁𝐎𝐓_🐼🪽",
     description: "Prevent admin changes",
 };
 
