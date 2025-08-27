88645a2c5b05a019661f757f0454f04fee68fefb
Monster convo <164199084+ARIYENKHAN01@users.noreply.github.com>
2025-08-28 02:14:21 +0530

    Update antiout.js

--- Script/events/antiout.js
+++ Script/events/antiout.js
@@ -2,7 +2,7 @@ module.exports.config = {
  name: "antiout",
  eventType: ["log:unsubscribe"],
  version: "0.0.1",
- credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+ credits: "𝐒𝐇𝐈𝐙𝐔𝐊𝐀_𝐁𝐎𝐓_🐼🪽",
  description: "Listen events"
 };
 
@@ -15,8 +15,8 @@ module.exports.run = async({ event, api, Threads, Users }) => {
  if (type == "self-separation") {
   api.addUserToGroup(event.logMessageData.leftParticipantFbId, event.threadID, (error, info) => {
    if (error) {
-    api.sendMessage(`সরি বস আবালরে এড় করতে পারলাম না \n ${name} এই আবালরে ব্লক করছে অথবা তার আইডিতে মেসেঞ্জার অপশন তাই এড করতে পারলাম না😞 \n\n ──────·····✦·····──── \n 𝗜𝘀𝗹𝗮𝗺𝗶𝗰𝗸 𝗰𝗵𝗮𝘁 𝗯𝗼𝘁 | ᵁᴸᴸ⁴ˢᴴ `, event.threadID)
-   } else api.sendMessage(`শোন, ${name} এই গ্রুপ হইলো গ্যাং! \n এখান থেকে যাইতে হইলে এড়মিনের ক্লিয়ারেন্স লাগে! \nতুই পারমিশন ছাড়া লিভ নিছোস – তোকে আবার মাফিয়া স্টাইলে এড় দিলাম। \n\n ── ·······✦·······──── \n 𝗜𝘀𝗹𝗮𝗺𝗶𝗰𝗸 𝗰𝗵𝗮𝘁 𝗯𝗼𝘁 | ᵁᴸᴸ⁴ˢᴴ `, event.threadID);
+    api.sendMessage(`𝐒𝐨𝐫𝐫𝐲 𝐛𝐨𝐬𝐬 𝐦𝐞𝐧𝐭𝐚𝐥 𝐤𝐨 𝐚𝐝𝐝 𝐧𝐡𝐢 𝐤𝐚𝐫 𝐩𝐚𝐲𝐢 \n ${name} 𝐘𝐞 𝐤𝐮𝐭𝐭𝐞 𝐛𝐥𝐨𝐜𝐤 𝐤𝐢𝐲𝐚 𝐲𝐚 𝐩𝐡𝐢𝐫 𝐢𝐝 𝐩𝐞 𝐦𝐞𝐬𝐬𝐚𝐠𝐞 𝐨𝐩𝐭𝐢𝐨𝐧 𝐧𝐡𝐢 𝐢𝐬𝐥𝐢𝐲𝐞 𝐧𝐡𝐢 𝐤𝐬𝐫 𝐩𝐚𝐲𝐢😞 \n\n ──────·····✦·····──── \n 𝗜𝘀𝗹𝗮𝗺𝗶𝗰𝗸 𝗰𝗵𝗮𝘁 𝗯𝗼𝘁 | ᵁᴸᴸ⁴ˢᴴ `, event.threadID)
+   } else api.sendMessage(`𝐒𝐮𝐧, ${name} 𝐘𝐞 𝐠𝐫𝐨𝐮𝐩 𝐦𝐞𝐫𝐚 𝐡! \n 𝐈𝐝𝐡𝐚𝐫 𝐬𝐞 𝐣𝐚𝐧𝐚 𝐡𝐨 𝐭𝐨 𝐦𝐨𝐦𝐨𝐬 𝐤𝐡𝐢𝐥𝐚𝐨 ! \n𝐓𝐮 𝐦𝐨𝐦𝐨𝐬 𝐤𝐡𝐢𝐥𝐚𝐲𝐞 𝐛𝐢𝐧𝐚 𝐥𝐞𝐚𝐯𝐞 𝐥𝐢𝐲𝐚 - 𝐢𝐬𝐥𝐢𝐲𝐞 𝐭𝐮𝐣𝐡𝐞 𝐣𝐚𝐦𝐞𝐞𝐧 𝐤𝐡𝐨𝐝𝐤𝐞 𝐝𝐮𝐛𝐚𝐫𝐚 𝐚𝐝𝐝 𝐤𝐢 𝐡𝐮। \n\n ── ·······✦·······──── \n 𝐒𝐇𝐈𝐙𝐔𝐊𝐀_𝐁𝐎𝐓_🐼🪽| ᵁᴸᴸ⁴ˢᴴ `, event.threadID);
   })
  }
 }
